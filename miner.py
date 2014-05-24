1c843a8be4d3f416964fd7c79d86bc65902e7981
Manuel Albarrán <weap88@gmail.com>
2014-05-24 20:48:06 +0200

    Added argument parser to miner.py

--- miner.py
+++ miner.py
@@ -1,11 +1,29 @@
-import yaml
+import argparse
 from multiprocessing import Process
-from shared.utils import build_components
+
+import yaml
+import json
+
+from shared.utils import build_components, build_component
 
 if __name__ == '__main__':
-    conf = yaml.load(open("conf.yml"))
-    publishers = build_components(conf, "publishers")
-    extractors = build_components(conf, "extractors")
-    process = [Process(**extractor(publishers)) for extractor in extractors]
-    [p.start() for p in process]
-    [p.join() for p in process]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--conf", help="publish the string you use here", default="conf.yml")
+    parser.add_argument("--publish", help="publish the string you use here")
+    parser.add_argument("--publisher", help="publish the string you use here", type=build_component)
+    args = parser.parse_args()
+
+    conf = yaml.load(open(args.conf))
+
+    if args.publisher:
+        publishers=[args.publisher]
+    else:
+        publishers = build_components(conf, "publishers")
+
+    if args.publish:
+        [p(json.loads(args.publish)) for p in publishers]
+    else:
+        extractors = build_components(conf, "extractors")
+        process = [Process(**extractor(publishers)) for extractor in extractors]
+        [p.start() for p in process]
+        [p.join() for p in process]
--- publishers/wubytes_publisher.py
+++ publishers/wubytes_publisher.py
@@ -18,14 +18,15 @@ class WubytesPublisher(BasePublisher):
             self.wc.new_wu({'data': 0, 'title': key, 'slug': key})
 
     def publish(self, message):
+        try:
+            for key, path in self.keys.iteritems():
+                value = message
+                for k in path.split("/"):
+                    value = value[k]
 
-        for key, path in self.keys.iteritems():
-            value = message
-            for k in path.split("/"):
-                value = value[k]
 
+                print key, path, value
 
-            print key, path, value
-
-            self.wc.update_value(key, value)
-            
+                self.wc.update_value(key, value)
+        except KeyError:
+            pass
