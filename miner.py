452786a58449cdece09d157e8589c895f3b8b449
Manuel Albarrán <weap88@gmail.com>
2014-05-25 19:05:24 +0200

    Added password option for wubytes in conf.yml

--- miner.py
+++ miner.py
@@ -7,6 +7,11 @@ import json
 from shared.utils import build_components, build_component
 
 if __name__ == '__main__':
+
+    print "Visit this!"
+    print "http://wubytes.com/w/grid/example:cpu,swap,memory,procs,networkout,networkin"
+    print
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--conf", help="publish the string you use here", default="conf.yml")
     parser.add_argument("--publish", help="publish the string you use here")
--- publishers/wubytes_publisher.py
+++ publishers/wubytes_publisher.py
@@ -2,16 +2,20 @@ from base_publisher import BasePublisher
 from pprint import pprint as pp
 from wu_client import WuClient
 
-print "password for wubytes:",
-password = raw_input().strip()
+def get_password(conf):
+    password = conf.get("pass", None) or conf.get("password", None)
+    if not password:
+        print "Password for wubytes:",
+        password = raw_input().strip()
+    return password
 
 class WubytesPublisher(BasePublisher):
     def __init__(self, conf):
 
         self.keys = conf["keys"]
 
-        self.wc = WuClient(conf["client_id"], conf["client_secret"])
-        if not self.wc.auth(conf["username"], password):
+        self.wc = WuClient(conf["client_id"], conf["client_secret"], conf["host"])
+        if not self.wc.auth(conf["username"], get_password(conf)):
             exit()
 
         for key in self.keys:
