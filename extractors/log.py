bea9840accb09f57955b8f584c33e69135d8b7ab
Manuel Albarrán <weap88@gmail.com>
2014-05-30 23:47:26 +0200

    Add option to tail over ssh, and improve nginx filter

--- extractors/log.py
+++ extractors/log.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from sh import tail, touch, stat
+from sh import tail, touch, stat, ssh
 import re
 import time
 
@@ -13,25 +13,37 @@ class Log(BaseExtractor):
         before = conf.get("break_only_before", None)
         if merge:
             before = re.compile(before)
+        
+        value = HashBuilder({
+                "type": conf.get("type", "log"),
+                "source_path": conf["path"],
+                "tags": conf.get("tags", [])
+            })
 
-        value = HashBuilder({"type": conf.get("type", "log"),"source_path": conf["path"], "tags": conf.get("tags", [])}, fake_timestamp=conf["fake_timestamp"])
-
-        touch(conf["path"])
-
-        if conf.get("from", None) == "now":
-            filesize = "-c0"
+        if conf.get("host"):
+            host = "%s@%s" % (conf["user"], conf["host"]) if conf.get("user") else conf["host"]
+            remote_server = ssh.bake(host)
+            mac = str(ssh(host, "ifconfig | grep 'eth0'"))
+            value.shared["source_host"] = host
+            value.shared["source_mac"] = re.findall("..:..:..:..:..:..", mac)[0]
+            log = remote_server.tail("-f", conf["path"], _iter=True)
         else:
-            filesize = "-c%s" % (int(stat("-nf" ,'%z', conf["path"])) * 2 + 10)
+            touch(conf["path"])
+            if conf.get("from", None) == "now":
+                filesize = "-c0"
+            else:
+                filesize = "-c%s" % (int(stat("-nf" ,'%z', conf["path"])) * 2 + 10)
+            log = tail("-F", filesize, conf["path"], _iter=True)
 
-        for n, line in enumerate(tail("-F", filesize, conf["path"], _iter=True)):
+        for n, line in enumerate(log):
             if merge:
                 if before.match(line):
                     if value.has_data():
                         yield value.dict()
-                        time.sleep(0.1)
+                        # time.sleep(0.1)
                     value.build()
                 value.log(line)
             else:
                 value.build(line)
                 yield value.dict()
-                time.sleep(0.1)
+                # time.sleep(0.1)
