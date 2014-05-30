b0b2cee619a4c024d668f9dfcebda004b64ff186
Manuel Albarrán <weap88@gmail.com>
2014-05-30 20:38:23 +0200

    Added not-equal, in, and not-in operators for queries. And support for multiple operators

--- shared/query.py
+++ shared/query.py
@@ -1,20 +1,31 @@
 from shared.path import Path
+from copy import deepcopy
 
 class Checker:
     def __init__(self, value):
         self.value = value
 
     def _check(self, other):
-        if isinstance(self.value, dict):
-            if "$lt" in self.value:
-                return other < self.value["$lt"]
-            if "$gt" in self.value:
-                return other > self.value["$gt"]
-            if "$lte" in self.value:
-                return other <= self.value["$lte"]
-            if "$gte" in self.value:
-                return other >= self.value["$gte"]
-        return self.value == other
+        value = deepcopy(self.value)
+        ret = True
+        if isinstance(value, dict):
+            if "$lt" in value:
+                ret and other < value.pop("$lt")
+            if "$gt" in value:
+                ret = ret and other > value.pop("$gt")
+            if "$lte" in value:
+                ret = ret and other <= value.pop("$lte")
+            if "$gte" in value:
+                ret = ret and other >= value.pop("$gte")
+            if "$ne" in value:
+                ret = ret and other != value.pop("$ne")
+            if "$in" in value:
+                ret = ret and other in value.pop("$in")
+            if "$nin" in value:
+                ret = ret and other not in value.pop("$nin")
+        if value:
+            ret = ret and value == other
+        return ret
 
     def check(self, other):
         if isinstance(other, list):
