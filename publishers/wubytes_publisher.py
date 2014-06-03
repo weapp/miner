2639c6ce1647ff807e8dd30dfe15a2c9dfa7eeda
Manuel Albarrán <weap88@gmail.com>
2014-06-03 23:25:28 +0200

    remove unnecesary extra code in wuclient

--- publishers/wubytes_publisher.py
+++ publishers/wubytes_publisher.py
@@ -3,19 +3,13 @@ from pprint import pprint as pp
 from wu_client import WuClient
 from shared.path import Path
 
-def get_password(conf):
-    password = conf.get("pass", None) or conf.get("password", None)
-    if not password:
-        print "Password for wubytes:",
-        password = raw_input().strip()
-    return password
 
 class WubytesPublisher(BasePublisher):
     def __init__(self, conf):
         BasePublisher.__init__(self, conf)
 
         self.wc = WuClient(conf["client_id"], conf["client_secret"], conf["host"])
-        if not self.wc.auth(conf["username"], get_password(conf)):
+        if not self.wc.auth(conf["username"], conf["pass"]):
             exit()
 
         for key in self.project.project:
